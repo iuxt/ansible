92155abeecb1fad235f4cfcba489249c5dc522f6
zhanglikun <x@zahui.fan>
2023-08-02 17:20:11 +0800

    修复fcitx5-configtool无法启动的问题

--- roles/rime/files/tools/boot.sh
+++ roles/rime/files/tools/boot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-/usr/bin/mkdir -p /run/dbus/
-/usr/bin/dbus-daemon --system
+/usr/bin/fcitx5 --disable=wayland -d --verbose '*'=0
 
-
-daemonize -e /tmp/fcitx5.log -o /tmp/fcitx5.log -p /tmp/fcitx5.pid -l /tmp/fcitx5.pid -a /usr/bin/fcitx5 --disable=wayland
 export INPUT_METHOD=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=ibus
 
