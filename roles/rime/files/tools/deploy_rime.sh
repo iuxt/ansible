d5985f6bfc0e322dd0b87d4fcc27796098be1058
zhanglikun <x@zahui.fan>
2023-08-04 21:43:23 +0800

    优化wsl2使用fcitx5输入法

--- roles/rime/files/tools/deploy_rime.sh
+++ roles/rime/files/tools/deploy_rime.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+pkill fcitx5
 rime_deployer --build ~/.local/share/fcitx5/rime/ /usr/share/rime-data ~/.local/share/fcitx5/rime/build
+/usr/bin/fcitx5 --disable=wayland -d --verbose '*'=0
--- roles/rime/files/tools/sync_rime_dict.sh
+++ roles/rime/files/tools/sync_rime_dict.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
 pkill fcitx5
-
 cd ~/.local/share/fcitx5/rime/
 rime_dict_manager -s
-
-source ~/.zprofile
-
+/usr/bin/fcitx5 --disable=wayland -d --verbose '*'=0
